c6b11ef46a1b28abb95fdb95c9c3bbb80f9b8b21
Abhishek Meghwal <meghwalabhishek1@gmail.com>
2025-12-18 10:07:01 +0530

    Just a commit

--- src/agent/simple_agent.py
+++ src/agent/simple_agent.py
@@ -6,5 +6,19 @@ An agent loop is a program where LLM plans an action, your code executes it, and
 """
 
 
+from openai import OpenAI
+import json
+from src.config import OPENAI_API_KEY
+
+client = OpenAI(api_key= OPENAI_API_KEY)
+
+user_prompt = "Hi GPT"
+
+response = client.responses.create(
+    model= "gpt-4.1-mini",
+    input= user_prompt
+)
+
+print(response.output_text)
 
 
